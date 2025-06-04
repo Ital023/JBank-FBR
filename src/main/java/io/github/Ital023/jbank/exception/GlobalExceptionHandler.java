0429429b73727e914cc6d811b0695676ede5cfc0
Ital023 <contadoitalo911@gmail.com>
2025-06-04 00:37:21 -0300

    feat: tratamento de erros vindas do hibernate validator seguindo padrão

--- src/main/java/io/github/Ital023/jbank/exception/GlobalExceptionHandler.java
+++ src/main/java/io/github/Ital023/jbank/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package io.github.Ital023.jbank.exception;
 
+import io.github.Ital023.jbank.exception.dto.InvalidParamDto;
 import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -10,6 +12,22 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(JBankException.class)
     public ProblemDetail JBankException(JBankException e) {
         return e.toProblemDetail();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ProblemDetail handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+
+        var invalidParams = e.getFieldErrors()
+                .stream()
+                .map(fe -> new InvalidParamDto(fe.getField(), fe.getDefaultMessage()))
+                .toList();
+
+        var pd = ProblemDetail.forStatus(400);
+        pd.setTitle("Invalid request parameters");
+        pd.setDetail("There is invalid fields on the request");
+        pd.setProperty("invalid-params", invalidParams);
+
+        return pd;
     }
 
 
---
+++ src/main/java/io/github/Ital023/jbank/exception/dto/InvalidParamDto.java
@@ -0,0 +1,4 @@
+package io.github.Ital023.jbank.exception.dto;
+
+public record InvalidParamDto(String field, String reason) {
+}
