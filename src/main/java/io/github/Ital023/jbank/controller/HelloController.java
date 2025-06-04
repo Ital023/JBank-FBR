13b947e2fe58337dadac4fc2efe4fdfa6061f26c
Ital023 <contadoitalo911@gmail.com>
2025-06-03 22:28:41 -0300

    feat: adicionando os interceptors

---
+++ src/main/java/io/github/Ital023/jbank/controller/HelloController.java
@@ -0,0 +1,16 @@
+package io.github.Ital023.jbank.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping
+    public ResponseEntity<String> ok() {
+        System.out.println("aqui controller");
+        return ResponseEntity.ok().build();
+    }
+
+}
